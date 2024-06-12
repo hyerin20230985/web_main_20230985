d44ab4f4c458bab5b6fb1c9d5879adf5ee85b59d
hyerin <zeez4304@naver.com>
2024-06-12 18:38:50 +0900

    15주차완료

--- js/security.js
+++ js/security.js
@@ -1,35 +1,32 @@
-if (emailValue.length < 5) {
-    alert('아이디는 최소 5글자 이상 입력해야 합니다.');
-    return false;
-    }
-    if (passwordValue.length < 12) {
-    alert('비밀번호는 반드시 12글자 이상 입력해야 합니다.');
-    return false;
-    }
-    const hasSpecialChar = passwordValue.match(/[!,@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/) !== null;
-    if (!hasSpecialChar) {
-    alert('패스워드는 특수문자를 1개 이상 포함해야 합니다.');
-    return false;
-    }
-    const hasUpperCase = passwordValue.match(/[A-Z]+/) !== null;
-    const hasLowerCase = passwordValue.match(/[a-z]+/) !== null;
-    if (!hasUpperCase || !hasLowerCase) {
-    alert('패스워드는 대소문자를 1개 이상 포함해야 합니다.');
-    return false;
-    }
+function encodeByAES256(key, data){
+    const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
+       iv: CryptoJS.enc.Utf8.parse(""),
+       padding: CryptoJS.pad.Pkcs7,
+       mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString();
+}
+  function decodeByAES256(key, data){
+    const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
+      iv: CryptoJS.enc.Utf8.parse(""),
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString(CryptoJS.enc.Utf8);
+}
 
-    const check_xss = (input) => {
-        // DOMPurify 라이브러리 로드 (CDN 사용)
-        const DOMPurify = window.DOMPurify;
-        // 입력 값을 DOMPurify로 sanitize
-        const sanitizedInput = DOMPurify.sanitize(input);
-        // Sanitized된 값과 원본 입력 값 비교
-        if (sanitizedInput !== input) {
-        // XSS 공격 가능성 발견 시 에러 처리
-        alert('XSS 공격 가능성이 있는 입력값을 발견했습니다.');
-        return false;
-        }
-        // Sanitized된 값 반환
-        return sanitizedInput;
-        };
-        
+function encrypt_text(password){
+  const k = "key";
+  const rk = k.padEnd(32, " ");
+  const b = password;
+  const eb = this.encodeByAES256(rk, b);
+  return eb;
+  console.log(eb);
+}
+
+  function decrypt_text(){
+      const k = "key";
+      const rk = k.padEnd(32, " ");
+      const eb = session_get();
+      const b = this.decodeByAES256(rk, eb);
+  }
